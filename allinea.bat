2e28ae7c7c6a795fe2ebbe6e91246a602ea3db85
wiliamebook <wiliamebook@gmail.com>
2026-05-25 15:23:31 +0200

    Correzzioni

---
+++ allinea.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Correzzioni"
+git push origin master
---
+++ laprimavolta.bat
@@ -0,0 +1,13 @@
+cd C:\0-SW\Libro
+
+git init
+
+git config user.name "wiliamebook"
+git config user.email "wiliamebook@gmail.com"
+
+git add .
+git commit -m "Primo upload - landing page"
+
+git remote add origin https://github.com/wiliamebook/analisimonetaria.git
+
+git push -u origin master
